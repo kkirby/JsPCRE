ca3db7a274a18a1f2966c22f9975ac7301e21287
Kyle Kirby <kylekirby@optimalconnection.net>
2014-12-19 08:58:23 -0600

    Compiled with optimizations.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 PCRE_PATH=`./config.sh`
-emcc ./libpcre.dylib pcre.cc -I$PCRE_PATH -Wno-warn-absolute-paths --bind -o pcre.js
+emcc ./libpcre.dylib pcre.cc -I$PCRE_PATH -Wno-warn-absolute-paths --bind -O3 -o pcre.js
